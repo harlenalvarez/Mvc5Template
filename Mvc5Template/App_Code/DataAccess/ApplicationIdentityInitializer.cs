39007a36dd6a72fcd4cac0b6747d8389df08c0b5
Owner <Owner@GAMING-PC>
2016-05-17 22:28:39 -0400

    Changed Initializer

--- Mvc5Template/App_Code/DataAccess/ApplicationIdentityInitializer.cs
+++ Mvc5Template/App_Code/DataAccess/ApplicationIdentityInitializer.cs
@@ -1,4 +1,6 @@
-﻿using Mvc5Template.Models;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Mvc5Template.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -9,23 +11,41 @@ namespace Mvc5Template.App_Code.DataAccess
 {
     public class ApplicationIdentityInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
     {
-        public delegate void InitializerDelegate();
-        private InitializerDelegate initDB;
-
-        public void AddInit(InitializerDelegate initializer)
-        {
-            initDB += initializer;
-        }
-
-        public void RemoveInit(InitializerDelegate initializer)
-        {
-            initDB -= initializer;
-        }
+        
         
         protected override void Seed(ApplicationDbContext context)
         {
-            initDB();
+            InitializeUsersAndRoles(context);
             base.Seed(context);
+        }
+
+        private static void InitializeUsersAndRoles(ApplicationDbContext context)
+        {
+            if(!context.Roles.Any())
+            {
+                var rs = new RoleStore<IdentityRole>(context);
+                var rm = new RoleManager<IdentityRole>(rs);
+                var role = new IdentityRole("Admin");
+                var userRole = new IdentityRole("User");
+                rm.Create(role);
+                rm.Create(userRole);
+            }
+            if (!context.Users.Any())
+            {
+                var us = new UserStore<ApplicationUser>(context);
+                var um = new UserManager<ApplicationUser>(us);
+                var adminUser = new ApplicationUser
+                {
+                    UserName = "harlenalvarez@gmail.com",
+                    PhoneNumber = "813-555-5555",
+                    Email = "harlenalvarez@gmail.com"
+
+                };
+
+                um.Create(adminUser, @"!@34QWer");
+                um.SetLockoutEnabled(adminUser.Id, true);
+                um.AddToRole(adminUser.Id, "Admin");
+            }
         }
 
        
